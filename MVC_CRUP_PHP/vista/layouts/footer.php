5c9cfb8ef0071f3e06c37e88ae412106a9c4e134
Dannyb8a <133927920+Dannyb8a@users.noreply.github.com>
2023-05-17 19:42:38 -0500

    Add files via upload

---
+++ MVC_CRUP_PHP/vista/layouts/footer.php
@@ -0,0 +1,10 @@
+       <!-- /contenido-->
+
+       <h1 class="text-center">PARA MODIFICAR EL PIE</h1>
+       <h1 class="text-center">DE LA PAGINA</h1>
+
+           
+       </div> 
+
+</body>
+</html>
---
+++ MVC_CRUP_PHP/vista/layouts/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DANNY</title>
+    <link rel="stylesheet" href="vista/css/app.css">
+</head>
+<body>
+   <div class="panel">
+       <h1 class="text-center">MICRO MERCADO DE TODITO</h1>
+       <h1 class="text-center">GESTIÓN DE PRODUCTOS</h1>
+      
+
+
+
+
+       <!-- contenido-->
